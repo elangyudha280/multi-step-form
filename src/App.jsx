c9a3ef07184bbb1b7ee527534c37b6d5868e1b46
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-08-03 14:44:00 +0700

    buat router ke page select plan

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import { Routes,Route } from 'react-router-dom'
 
 // import component
 import PagePersonalInfo from './pages/personalInfo'
+import SelectPlan from './pages/selectPlan'
+
 function App() {
  
 
   return (
       <Routes>
+        {/* router to personal info */}
         <Route path='/' element={<PagePersonalInfo/>} />
+        {/* router to select your plan */}
+        <Route path='/selectPlan' element={<SelectPlan/>} />
       </Routes>
   )
 }
---
+++ src/pages/selectPlan.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import layout
+import FormLayout from "../layout/formLayout";
+
+const SelectPlan = ()=>{
+    return (
+        <FormLayout title='Select Your Plan'>
+
+        </FormLayout>
+    )
+}
+
+export default SelectPlan;
