5be43fb2ff3eb83e659a69662c491328cc3bf1a9
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-08-16 20:34:45 +0700

    selesai desain form 4

--- src/pages/finishingUp.jsx
+++ src/pages/finishingUp.jsx
@@ -11,7 +11,7 @@ const FinishingUpPage = ()=>{
         <FormLayout title='Finishing Up' step='4'>
               <section className="main_content_form ">
                 {/* container form */}
-                <section className="container_form border-2 border-red-600 flex-1 w-full h-full px-5 md:px-2">
+                <section className="container_form flex-1 w-full h-full px-5 md:px-2">
                     {/* main form4 */}
                     <section className="main_form">
                         <h2 className="title-form text-[30px] font-bold text-primary-marine-blue">
@@ -21,8 +21,9 @@ const FinishingUpPage = ()=>{
                             double-check  everything looks OK before confirming. 
                         </p>
 
-                        <section className="container_finshing_up w-full border-2 overflow-hidden rounded-md border-red-600 mt-4">
-                                <div className="finishing_header w-full px-4 py-2 bg-netral-magnolia">
+                        <section className="container_finshing_up w-full  overflow-hidden   mt-4">
+                                <div className="finishing_header w-full px-4 py-2 bg-netral-magnolia rounded-md">
+                                    {/* select plan */}
                                     <div className="select_select_plan_info w-full flex gap-2 items-center py-4 border-b-2 border-b-neutral-200">
                                             <h2 className="title_select_plan flex-1 w-full text-[14px] font-semibold text-primary-marine-blue">
                                                 Arcade (Monthly)
@@ -31,6 +32,35 @@ const FinishingUpPage = ()=>{
                                             $9/mo
                                             </h2>
                                     </div>
+                                    {/* add ons  */}
+                                    <div className="add_ons_info mt-1 w-full flex flex-col ">
+                                        <div className="py-2 w-full flex items-center">
+                                            <h2 className="title_add_ons flex-1 w-full text-[13px] font-medium text-netral-cool-gray">
+                                                online service
+                                            </h2>
+                                            <h2 className="price_add_ons text-[13px] font-bold text-primary-marine-blue ">
+                                            $9/mo
+                                            </h2>
+                                        </div>
+                                         <div className="py-2 w-full flex items-center">
+                                            <h2 className="title_add_ons flex-1 w-full text-[13px] font-medium text-netral-cool-gray">
+                                                larger storage
+                                            </h2>
+                                            <h2 className="price_add_ons text-[13px] font-bold text-primary-marine-blue ">
+                                            $9/mo
+                                            </h2>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                </div>
+                                <div className="finishin_body w-full flex items-center px-4 py-3 ">
+                                    <h2 className="title_total_price flex-1 w-full text-[16px] font-medium text-netral-cool-gray">
+                                        Total
+                                    </h2>
+                                    <h2 className="total_price text-[20px] truncate font-bold text-primary-purplish-blue">
+                                        +$12 
+                                    </h2>
                                 </div>
                         </section>
                     </section>
