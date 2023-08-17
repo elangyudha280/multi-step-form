50beab7451c95307d5c4043b707664c92d9df9e0
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-08-17 10:51:38 +0700

    set layoutinh successPage

---
+++ src/pages/successPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import layout
+import FormLayout from "../layout/formLayout";
+
+
+
+const SuccessPage = ()=>{
+    return (
+        <FormLayout title='success' step='4'>
+
+        </FormLayout>
+    )
+}
+
+export default SuccessPage
